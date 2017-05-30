590a659ced3f543670312f540244aa51b64e40a6
Rashidul Hasan <shaikot@Rashiduls-MacBook-Pro.local>
2017-05-30 20:33:47 +0600

    basic application

---
+++ codeexercise/codeexercise/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  codeexercise
+//
+//  Created by Rashidul Hasan on 5/30/17.
+//  Copyright © 2017 Rashidul Hasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ codeexercise/codeexercise/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  codeexercise
+//
+//  Created by Rashidul Hasan on 5/30/17.
+//  Copyright © 2017 Rashidul Hasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
